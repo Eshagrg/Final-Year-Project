4ee21bec6e8869557c26837fc36fceca59c76d13
Eshagrg <np01cp4a210015@islingtoncollege.edu.np>
2024-02-29 22:52:50 +0545

    Úpdate .gitignore

--- MilijuliFurniture/obj/Debug/net6.0/MilijuliFurniture.AssemblyInfo.cs
+++ MilijuliFurniture/obj/Debug/net6.0/MilijuliFurniture.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MilijuliFurniture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6fcb96891725a7e09c4b9345583223c31c2d09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MilijuliFurniture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MilijuliFurniture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
