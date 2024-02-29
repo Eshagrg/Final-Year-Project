2c50022432189ac860fbd93a3b222ec836a0d12b
Eshagrg <np01cp4a210015@islingtoncollege.edu.np>
2024-02-29 20:10:19 +0545

    Project Start
    Created the project for POS for Milijuli Furniture

---
+++ MilijuliFurniture/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MilijuliFurniture.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ Site.DataAccess/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Site.DataAccess
+{
+	public class Class1
+	{
+
+	}
+}
